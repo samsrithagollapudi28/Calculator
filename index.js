e4167b16c8c9e78bb713d91404bf00f24eedf858
samsrithagollapudi28 <55306899+samsrithagollapudi28@users.noreply.github.com>
2020-01-25 19:04:51 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+function display(val)
+{
+document.getElementById("result").value+=val;
+}
+function solve()
+{
+var x=document.getElementById("result").value;
+var y=eval(x);
+document.getElementById("result").value=y;
+}
+function clr()
+{
+document.getElementById("result").value="";
+}
